239cc0c51902a594b6f4b7a18a4a99929508807e
eshika <agarwaleshi@gmail.com>
2021-04-09 19:39:33 +0530

    added board api

--- gui.py
+++ gui.py
@@ -1,21 +1,57 @@
 import sys, pygame as pg
+import requests
 
 pg.init()
+response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
+grid = response.json()['board']
+number_grid = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
 screen_size = 750, 750
 screen = pg.display.set_mode(screen_size)
+pg.display.set_caption("Sudoku")
+font = pg.font.SysFont(None, 35)
+
+
+# number_grid = [
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# ]
 
 def draw_background():
     screen.fill(pg.Color("lavender"))
+    #draw outside square
     pg.draw.rect(screen, pg.Color("black"), pg.Rect(15, 15, 720, 720), 10)
     i = 1
     while (i * 80) < 720:
+        line_width = 1 if i % 3 > 0 else 7
         #draw vertical lines
-        pg.draw.line(screen, pg.Color("black"), pg.Vector2((i * 80)+ 15, 15), pg.Vector2((i * 80) + 15, 735), 5)
+        pg.draw.line(screen, pg.Color("black"), pg.Vector2((i * 80)+ 15, 15), pg.Vector2((i * 80) + 15, 735), line_width)
         #draw horizontal lines
-        pg.draw.line(screen, pg.Color("black"), pg.Vector2(15, (i * 80)+ 15), pg.Vector2(735, (i * 80) + 15), 5)
+        pg.draw.line(screen, pg.Color("black"), pg.Vector2(15, (i * 80)+ 15), pg.Vector2(735, (i * 80) + 15), line_width)
 
         i += 1
 
+def draw_numbers():
+    row = 0
+    offset = 48
+    while row < 9:
+        col = 0
+        while col < 9:
+            output = grid[row][col]
+            # print(str(output))
+
+            if(0<output<10):
+                n_text = font.render(str(output), True, pg.Color("black"))
+                screen.blit(n_text, pg.Vector2((col * 80) + offset, (row * 80) + offset))
+            col += 1
+
+        row += 1
 
 
 def game_loop():
@@ -23,6 +59,7 @@ def game_loop():
         if event.type == pg.QUIT: sys.exit()
 
     draw_background()
+    draw_numbers()
     pg.display.flip()
 
 
